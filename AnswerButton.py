65a2840dfb327fd0ba6ac33bad4e796c17a42f60
mpn3712 <mpn3712@rit.edu>
2013-11-11 12:46:17 -0500

    put answer button in it's own file

---
+++ AnswerButton.py
@@ -0,0 +1,13 @@
+import pygame
+
+class AnswerButton(Button):
+    def __init__(self, x, y, width, height, filename, text=''):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.image = pygame.image.load(filename)
+        self.text = text
+    
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
--- FractionautsMain.py
+++ FractionautsMain.py
@@ -41,19 +41,6 @@ class Button:
             return pos
         else:
             return None
-
-
-class AnswerButton(Button):
-    def __init__(self, x, y, width, height, filename, text=''):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.image = pygame.image.load(filename)
-        self.text = text
-    
-    def draw(self, screen):
-        screen.blit(self.image, (self.x, self.y))
         
 
 class Question:
