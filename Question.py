bba2c4464b26e1798f87354b2d3a4228a18d2bfc
Lance Laughlin <lancealaughlin@gmail.com>
2013-11-12 16:24:06 -0500

    Question class created for addition - other operations will come after first level is completed

--- Question.py
+++ Question.py
@@ -6,14 +6,18 @@ import random
 class Question:
 
     def __init__(self, questionType):
+        #The number of total answers
         numChoices = 6
+        #Give us a random number representing the number of correct answers
         numCorrect = random.randint(2, 4)
         correctAnswers = []
         incorrectAnswers = []
         print numCorrect
 
+        #break 1 into x amount of sections(number of correct answers)
         section = float(1) / float(numCorrect)
 
+        #generate the correct answers (pulls out a chunk from section)
         for i in range(0, numCorrect):
             fraction = round(random.uniform(0.1, section), 2)
             correctAnswers.append(fraction)
@@ -21,12 +25,15 @@ class Question:
 
         print correctAnswers
 
+        #add correct answers together to get goal decimal
         goal = sum(correctAnswers)
         print goal
 
+        #how many incorrect answers should we generate? (toal number of questions minus correct answers)
         dummyQuestions = numChoices - numCorrect
         print dummyQuestions
 
+        #generate dummy questions
         for i in range(0, dummyQuestions):
             fraction = round(random.uniform(0.1, 0.9), 2)
             incorrectAnswers.append(fraction)
